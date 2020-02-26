72644bcc896f48eae22c5ebfd4a864bd8e4221f5
liujy <liujy@linkedge.com.cn>
2020-02-26 14:57:25 +0800

    update dist

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/resume/dist/',
 
     /**
      * Source Maps
